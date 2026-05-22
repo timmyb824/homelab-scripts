daf00741749d9cd772c69a936469bafe7b7e3688
Tim Bryant <timothybryant3@gmail.com>
2026-05-21 21:39:36 -0400

    refactor: reorganize folders

---
+++ scripts/nas/sync_to_nas.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rsync -av --delete /Users/timothybryant/DEV/projects/ /Volumes/filebrowser/macbook-projects/
+rsync -av --delete /Users/timothybryant/Documents/notes/ /Volumes/filebrowser/machook-homelab/
+# add more as needed
