b1d509f4e456074b99f054817ee05113f58497c7
Tim Bryant <timothybryant3@gmail.com>
2026-05-30 14:16:05 -0400

    fix: specify full path for rsync command in sync_to_nas.sh script

--- scripts/nas/sync_to_nas.sh
+++ scripts/nas/sync_to_nas.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rsync -av --delete /Users/timothybryant/DEV/projects/ /Volumes/filebrowser/macbook-projects/ &&
+/usr/bin/rsync -av --delete /Users/timothybryant/DEV/projects/ /Volumes/filebrowser/macbook-projects/ &&
     curl -m 10 --retry 5 "https://healthchecks.timmybtech.com/ping/${HC_PING_ID}"
